f11858e2491ac01199e9eb3eb514624a5471bea7
Ian <ianelicha@gmail.com>
2022-05-08 00:34:43 -0600

    Default values for ActionError class constructor

--- src/error.ts
+++ src/error.ts
@@ -1,7 +1,11 @@
-type ActionType = "response" | "reaction";
+type ActionType = "response" | "reaction" | "unknown";
 
 export class ActionError extends Error {
-  constructor(public type: ActionType, message?: string, public e?: any) {
+  constructor(
+    public type: ActionType,
+    message: string = "",
+    public e: any = new Error()
+  ) {
     super(message);
   }
 }
